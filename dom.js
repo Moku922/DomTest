034433fd968cd65019a2f5503e1eb11995f420a6
Moku922 <abdulmoqtadir1@gmail.com>
2023-01-09 01:22:19 +0530

    querySelector && querySelectorAll

--- dom.js
+++ dom.js
@@ -7,20 +7,20 @@ headerTitle.innerText = "Hello"
 headerTitle.style.border = "solid 3px #000"
 
 let title = document.getElementsByClassName("title")[0];
-console.log(title);
+// console.log(title);
 title.style.color = "green"
 title.style.fontWeight = "900"
 
 
 // Get Elements by Class
 
-let item = document.getElementsByClassName("list-group-item");
+// let item = document.getElementsByClassName("list-group-item");
 
-item[2].style.background = "green"
+// item[2].style.background = "green"
 
-for (let i = 0; i<item.length; i++){
-    item[i].style.fontWeight = "900"
-}
+// for (let i = 0; i<item.length; i++){
+//     item[i].style.fontWeight = "900"
+// }
 
 // Get Element by tag
 
@@ -28,6 +28,37 @@ let li = document.getElementsByTagName("li");
 
 for (let i = 0; i < li.length; i++){
     li[i].style.background = "#f4f4f4"
-}
+};
 
+// Query Selector
 
+// let item = document.querySelector(".list-group-item");
+
+// item.style.color = "coral";
+
+// let secondItem = document.querySelector(".list-group-item:nth-child(2)");
+
+// secondItem.style.background = "green";
+
+// let thirdItem = document.querySelector(".list-group-item:nth-child(3)");
+
+// thirdItem.style.display = "none";
+
+// querySelectorAll
+
+let Item = document.querySelectorAll(".list-group-item");
+
+Item[1].style.color = "green";
+
+let odd = document.querySelectorAll(".list-group-item:nth-child(odd)");
+
+for(let i=0; i < odd.length; i++){
+    odd[i].style.backgroundColor = "green"
+};
+
+// let even = document.querySelectorAll(".list-group-item:nth-child(even)");
+
+// for (let i = 0; i < even.length; i++) {
+//     even[i].style.backgroundColor = "yellow"
+    
+// }
