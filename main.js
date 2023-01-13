3ff3b7706b3b4f5b3971b861ef8f708c5774e208
Moku922 <abdulmoqtadir1@gmail.com>
2023-01-13 23:05:41 +0530

    Task 11 == Make user data into Object

--- main.js
+++ main.js
@@ -5,9 +5,26 @@ const form = document.querySelector("#my-form");
 form.addEventListener("submit", storeData);
 
 // function
+// function storeData(e){
+//     e.preventDefault();
+//     let userName = document.querySelector("#name").value;
+//     let userEmail = document.querySelector("#email").value;
+//     localStorage.setItem(userName,userEmail);
+// }
 function storeData(e){
     e.preventDefault();
+
     let userName = document.querySelector("#name").value;
     let userEmail = document.querySelector("#email").value;
-    localStorage.setItem(userName,userEmail);
+    let myObj = {
+        name: userName ,
+        email: userEmail 
+    }
+    let myObjString = JSON.stringify(myObj);
+    localStorage.setItem(userName,myObjString);
+
+    let myObjDestring = JSON.parse(myObjString);
+
+
+
 }
