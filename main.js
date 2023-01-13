c423d0fcbd000907c005722e4764258f39dbf3d4
Moku922 <abdulmoqtadir1@gmail.com>
2023-01-13 23:32:25 +0530

    Task 12 == Showing User in Display using li

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const form = document.querySelector("#my-form");
+const users = document.querySelector("#users")
 
 
 // add event listener
@@ -16,9 +17,19 @@ function storeData(e){
 
     let userName = document.querySelector("#name").value;
     let userEmail = document.querySelector("#email").value;
+    let userPhone = document.querySelector("#mNumber").value;
+// adding userData into screen
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(userName));
+    li.appendChild(document.createTextNode(" - "+userEmail));
+    li.appendChild(document.createTextNode(" - "+userPhone));
+    users.appendChild(li);
+
+
     let myObj = {
         name: userName ,
-        email: userEmail 
+        email: userEmail ,
+        phone_number: userPhone
     }
     let myObjString = JSON.stringify(myObj);
     localStorage.setItem(userName,myObjString);
